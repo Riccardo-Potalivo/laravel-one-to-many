6d8606e3c99fae28341ff0901b340dc5740fab6b
Riccardo Potalivo <riccardopotalivo@gmail.com>
2024-01-16 17:05:16 +0100

    relation one-to-many (Project & Type) set

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,9 @@ class Project extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'slug', 'name', 'description', 'repository', 'image'];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
